611fa26741298debea7c7fa5203f6a61c86ba835
MartinKoeoep <you@example.com>
2018-10-18 12:27:55 +0100

    Added content to aboutUs, buyHoney and landingPage. Minor styling changes

--- js/app.js
+++ js/app.js
@@ -1,21 +1,32 @@
 $(document).ready(function() {
 
+  const $video = $('#landingPageVideo');
+
   const $navLink = $('.navLink');
   const $dropdown = $('.dropdown');
   const idArray = ['landingPage','buyHoney','sellHoney','submitResearch','discoverResearch','howItWorks','aboutUs'];
-  const initialLanding = window.location.href.split('#landingPage').pop();
+  const initialLanding = window.location.href.split('#').pop();
+  console.log(initialLanding);
 
   $navLink.click( (e) => {
-    const clickTarget = e.target.href.split('#').pop();
+    let clickTarget;
+    if (e.target.tagName === 'IMG') {
+      clickTarget = e.currentTarget.href.split('#').pop();
+      sectionToggler(clickTarget);
+      $video[0].play();
+    } else {
+      clickTarget = e.target.href.split('#').pop();
+      $video[0].pause();
+    }
     sectionToggler(clickTarget);
+    $('.navbar-burger').toggleClass('is-active');
+    $('.navbar-menu').toggleClass('is-active');
   });
 
   function sectionToggler(target){
     $(`#${target}`).removeClass('hide');
-    $(`a[href="#${target}"]`).addClass('active');
     const filteredList = idArray.filter( item => item !== target);
     filteredList.forEach(item => {
-      $(`a[href="#${item}"]`).removeClass('active');
       $(`#${item}`).addClass('hide');
     });
   }
